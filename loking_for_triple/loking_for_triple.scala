53659a4ebeac7ec6eed3fc1ea492915f51f31b45
James lee <34915951+yahvz01@users.noreply.github.com>
2023-02-10 02:38:02 +0900

    Update loking_for_triple.scala

--- loking_for_triple/loking_for_triple.scala
+++ loking_for_triple/loking_for_triple.scala
@@ -1,4 +1,4 @@
-def isTriple( first : Int )( second : Int )( third : Int ) = {
+def isTriple( first : Int, second : Int, third : Int ) = {
     val sum = first + second + third
     if( sum == 0 ) true else false
 }
@@ -6,16 +6,29 @@ def isTriple( first : Int )( second : Int )( third : Int ) = {
 // using curring function for optimization in scala 3
 def solution(number: Vector[Int]): Int = {
     var combination = 0
-    for first <- 0 until ( number.size - 2)
-    do 
-        for second <- (first + 1) until (n umber.size - 1)
-        do
-            for third <- (second + 1) until ( number.size )
-            do
-                if  isTriple(number(first), number(second), number(third))
-                then combination += 1
+    for
+        first <- 0 until ( number.size - 2 )
+        second <- (first + 1) until ( number.size - 1 )
+        third <- (second + 1) until ( number.size )
+        if isTriple(number(first), number(second), number(third))
+    do
+        combination += 1
     return combination
 }
+
+// 설명을 위한 풀어쓰기
+// def solution(number: Vector[Int]): Int = {
+//     var combination = 0
+//     for first <- 0 until ( number.size - 2)
+//     do 
+//         for second <- (first + 1) until (number.size - 1)
+//         do
+//             for third <- (second + 1) until ( number.size )
+//             do
+//                 if  isTriple(number(first), number(second), number(third))
+//                 then combination += 1
+//     return combination
+// }
 
 // for scala 2 compiler in programmers
 // def solution(number: Vector[Int]): Int = {
